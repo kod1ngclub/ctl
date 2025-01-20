8f86fca2d90ecdad914a901c081cdace1b81f0ee
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-21 06:20:07 +0900

    FEAT: Add some shared types 1. bool 2. str

---
+++ include/shared/bool.h
@@ -0,0 +1,12 @@
+#ifndef CTL_SHARED_BOOL_H
+#define CTL_SHARED_BOOL_H
+
+#include "stdbool.h"
+
+/*
+ * Import '_Bool' type from stdbool
+ * But 'bool' is already defined in stdbool, so need no re-typedef
+ * If stdbool changes, so 'bool' disapeears, use typedef to define 'bool'
+*/
+
+#endif // CTL_SHARED_BOOL_H
