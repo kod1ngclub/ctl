e72abbe884bab8d11d8c399a0f8bd8c36ffa9349
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-22 19:19:06 +0900

    FEAT: Add some default semantic macros - succ: for succesive values (str is  succsive char) - arrow: for just-pointing pointer - ref: const reference - cast: type-casting

---
+++ include/default.h
@@ -0,0 +1,10 @@
+#ifndef CTL_DEFAULT_H
+#define CTL_DEFAULT_H
+
+#define succ(t) t* const
+#define arrow(t) const t*
+#define ref(t) const t* const
+
+#define cast(t, v) (t)(v)
+
+#endif // CTL_DEFAULT_H
