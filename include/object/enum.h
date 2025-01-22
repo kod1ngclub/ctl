257d67b342360b9da193ccd6809e1a2b1f03a2ec
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-22 21:36:20 +0900

    FIX: fix miss typings

--- include/object/enum.h
+++ include/object/enum.h
@@ -1,8 +1,8 @@
 #ifndef CTL_OBJECT_ENUM_H
 #define CTL_OBJECT_ENUM_H
 
-#define enum(n)                        \
-    typedef struct __enum_##n##_t__ n; \
-    struct __enum_##n##_t__            \
+#define enum(n)                         \
+    typedef enum __enum_##n##_t__ n;    \
+    enum __enum_##n##_t__               \
 
 #endif // CTL_OBJECT_ENUM_H
