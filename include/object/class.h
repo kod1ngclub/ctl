d7b04ecafd93ae82648153f15c32471ee9df12ee
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-22 19:29:32 +0900

    FIX: Add missed 'struct' to typedef

--- include/object/class.h
+++ include/object/class.h
@@ -1,9 +1,9 @@
 #ifndef CTL_OBJECT_CLASS_H
 #define CTL_OBJECT_CLASS_H
 
-#define class(n)                    \
-    typedef __class_##n##_t__ n;    \
-    struct __class_##n##_t__        \
+#define class(n)                        \
+    typedef struct __class_##n##_t__ n; \
+    struct __class_##n##_t__            \
 
 #define fn(n, ...) (*n)(__VA_ARGS__)
 
