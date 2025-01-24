b82ce93a96d75f4cc5e85fab96064204e6cdde2b
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-24 23:26:27 +0900

    FIX: Fixed 'char* const' to 'str' in option.h (for more semantic typing)

--- include/util/option.h
+++ include/util/option.h
@@ -1,13 +1,15 @@
 #ifndef CTL_UTIL_OPTION_H
 #define CTL_UTIL_OPTION_H
 
+#include "shared/str.h"
+
 #define option(t, e) __option_##t##_##e##__
 
-#define typedef_option(t, e)        \
-    typedef struct {                \
-        const t data;               \
-        const e err;                \
-        const char* const body;     \
-    } option(t, e)                  \
+#define typedef_option(t, e)    \
+    typedef struct {            \
+        const t data;           \
+        const e err;            \
+        const str body;         \
+    } option(t, e)              \
 
 #endif // CTL_UTIL_OPTION_H
